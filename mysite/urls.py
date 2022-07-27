5c125dfd8ddcf95f0436ed4e6eca07559db0ec00
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-07-27 22:21:02 +0000

    more templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('tweets/<int:id>/', views.tweets, name='tweets')
+  path('tweets/<int:id>/', views.tweets, name='tweets'),
+  path('template/', views.template, name='template'),
+  path('dynamic/<str:name>/', views.dynamic, name='dynamic')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
   return HttpResponse("Hello, world. You're at the polls index")
 
 def tweets(request, id):
   return HttpResponse(f"Your post: {id}")
+
+def template(request):
+  return render(request, 'index.html', {})
+
+def dynamic(request, name):
+  pokemons = [
+    {
+      "id": 1,
+      "name": "Charizard"
+    },
+    {
+      "id": 2,
+      "name": "Bulbasur"
+    },
+  ]
+  context = {
+    "name": name,
+    "pokemons": pokemons
+  }
+  return render(request, 'dynamic.html', context)
